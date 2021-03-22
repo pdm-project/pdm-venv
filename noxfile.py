e70464c72aeef9cea37de3c5065a023574d36e95
Frost Ming <mianghong@gmail.com>
2021-03-22 18:03:25 +0800

    Update ci workflow

--- noxfile.py
+++ noxfile.py
@@ -6,7 +6,7 @@ os.environ["PDM_IGNORE_SAVED_PYTHON"] = "1"
 
 
 @nox.session(python=("3.7", "3.8", "3.9"))
-@nox.parametrize(backend=("virtualenv", "venv", "conda"))
+@nox.parametrize("backend", ("virtualenv", "venv", "conda"))
 def test(session, backend):
     os.environ["DEFAULT_BACKEND"] = backend
     session.run("pdm", "install", "-d", external=True)
