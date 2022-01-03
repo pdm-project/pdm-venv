8232335a0f1435cc8181652a546121e655f9e5b1
Frost Ming <mianghong@gmail.com>
2022-01-03 22:18:49 +0800

    see verbose output

--- noxfile.py
+++ noxfile.py
@@ -9,5 +9,5 @@ os.environ["PDM_IGNORE_SAVED_PYTHON"] = "1"
 @nox.parametrize("backend", ("virtualenv", "venv", "conda"))
 def test(session, backend):
     os.environ["DEFAULT_BACKEND"] = backend
-    session.run("pdm", "install", "-d", external=True)
+    session.run("pdm", "install", "-vd", external=True)
     session.run("pytest", "tests/")
