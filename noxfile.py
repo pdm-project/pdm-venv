c08a076861e89fcfba6641c6f85106e2d32a681c
Frost Ming <mianghong@gmail.com>
2022-02-18 17:22:17 +0800

    add missing session

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,7 @@ import nox
 os.environ["PDM_IGNORE_SAVED_PYTHON"] = "1"
 
 
-@nox.session(python=("3.7", "3.8", "3.9"))
+@nox.session(python=("3.7", "3.8", "3.9", "3.10"))
 @nox.parametrize("backend", ("virtualenv", "venv", "conda"))
 def test(session, backend):
     os.environ["DEFAULT_BACKEND"] = backend
